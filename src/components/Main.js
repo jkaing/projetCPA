86d07c4a3de5ae09bf931c0146512df2aa9f58be
28707756 <Shiyao.Chen@etu.sorbonne-universite.fr>
2024-04-05 16:51:27 +0200

    page_d'acceuil

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,61 @@
 import React from 'react';
-//import { useHistory } from 'react-router-dom'; // 如果你使用了 React Router，可以使用 useHistory 来实现路由导航
+import { useState, useEffect } from 'react';
+//import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './Main.css'; // 导入样式文件
+import GamePage from './page/game/Game';
+import ShopPage from './page/shop/Shop';
+
 
 function Main() {
-    //const history = useHistory();
+//    const history = useHistory();
+    const[page,setPage]=useState("welcome_page");
+    const [isStart, setStart] = useState(false);
+ 
+    useEffect(() => {
+        console.log("page:", page);
+        console.log("isStart:", isStart);
+      }, [page, isStart]);
+      
+    const handleStartGame = (ev) => {
+        ev.preventDefault();
+        setStart(true);
+        setPage("game_page");
+        //console.log(page); // 在页面状态更新之前打印
+        //console.log(isStart); // 在isStart状态更新之前打印
+      }
+      
 
-    const handleStartGame = () => {
-        // 点击开始游戏按钮时触发的函数
-        // 可以在这里添加开始游戏逻辑
-        // 比如导航到游戏页面
-        //history.push('/game'); // 假设游戏页面的路由是 '/game'
+    const handleShop = (ev) => {
+        ev.preventDefault();
+        setStart(false);
+        setPage("shop_page");
+        //console.log(page);
+        //console.log(isStart);
+
     }
 
-    return (
-        <div className="main">
-            <h1>Welcome to Space Shooter Game</h1>
-            <p>Get ready to experience the thrill of space battles!</p>
-        </div>
-    );
+    //根据当前页面状态渲染不同的页面组件
+    const renderPage = () => {
+        switch (page) {
+            case "game_page":
+                return <GamePage />;
+            case "shop_page":
+                return <ShopPage />;
+            default:
+                return (
+                    <div className="main">
+                        <h1>Welcome to Space Shooter Game!</h1>
+                        <div className='different_page'>
+                            <button onClick={handleStartGame}>Start Game</button>
+                            <button onClick={handleShop}>Shop</button>
+                        </div>
+                    </div>
+                );
+        }
+    };
+
+    return renderPage();
+
 }
 
 export default Main;
---
+++ src/components/page/game/Game.js
@@ -0,0 +1,15 @@
+// Game.js
+import React from 'react';
+import './Game.css'; // 导入样式文件
+
+
+function Game() {
+    return (
+        <div className='game_page'>
+            <h1>Game Page</h1>
+            {/* 游戏页面的内容 */}
+        </div>
+    );
+}
+
+export default Game;
